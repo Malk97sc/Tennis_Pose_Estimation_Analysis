ca1d29550643dffd323f5466d5a5b67b11b19a2c
Malk97sc <malaksanchez09@gmail.com>
2025-09-12 15:54:50 -0500

    Main Function

--- tennis_yolo/main.py
+++ tennis_yolo/main.py
@@ -2,28 +2,38 @@ from pathlib import Path
 
 from utils import read_video, save_video
 from utils import DATA_DIR, RAW_DATA_DIR, MODELS_DIR
-from tracking import PlayerTracking
+from tracking import PlayerTracking, BallTracking
 
 def main():
     video_path = RAW_DATA_DIR / "input_video.mp4"
     output_path = Path(DATA_DIR) / "results" / "video"
     output_path.mkdir(parents=True, exist_ok=True)
     
-    model_path = MODELS_DIR / "yolo" / "yolov8x.pt"
+    player_model_path = MODELS_DIR / "yolo" / "yolov8x.pt"
+    ball_model_path = MODELS_DIR / "fine_tuning" / "ball_yolov8x" / "weights" / "best.pt"
     stub_path = MODELS_DIR / "tracker_stubs"    
 
     #read video
     video = read_video(video_path)    
 
     #players
-    player_track = PlayerTracking(model_path) #player tracking instance
-    player_dt = player_track.detect_player(video, 
-                                           read_stub = True,
-                                           stub_path = stub_path / "player_detection.pkl") #player detection
-    video = player_track.draw_boxes(video, player_dt) 
+    player_track = PlayerTracking(player_model_path) #player tracking instance
+    player_dt = player_track.detect_player(video, read_stub = True, stub_path = stub_path / "player_detection.pkl") #player detection
+    
+    #ball
+    ball_track = BallTracking(ball_model_path) #ball tracking instance
+    ball_dt = ball_track.detect_ball(video, read_from_stub = True, stub_path = stub_path / "ball_detection.pkl") #ball detection
+    interpolation_method = 'linear'
+    #order = 3 #this orders is only for Spline and Polynomial interpolation
+    ball_dt = ball_track.interpolate_ball(ball_dt, method = interpolation_method) #ball interpolation to improve the result
 
-    output_video_path = output_path / "output_video.avi"
-    save_video(video, output_video_path)
+    #draw boxes
+    out_video = player_track.draw_boxes(video, player_dt) 
+    out_video = ball_track.draw_boxes(video, ball_dt)
+
+    output_video_path = output_path / f"output_video_{interpolation_method}.avi"
+    save_video(out_video, output_video_path)
+    print(f"Save video in: {output_video_path}")
 
 if __name__ == "__main__":
     main()
