533c4c83436ca971f73c5d65e13fa8c3f1b6b51a
Malk97sc <malaksanchez09@gmail.com>
2025-09-14 00:37:19 -0500

    Pick Players Method

--- tennis_yolo/tracking/player_tracker.py
+++ tennis_yolo/tracking/player_tracker.py
@@ -5,6 +5,43 @@ import pickle
 class PlayerTracking:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
+    
+    def _get_center(self, box):
+        x1, y1, x2, y2 = box
+        center_x = (x1+x2) // 2
+        center_y = (y1+y2) // 2
+        return center_x, center_y
+    
+    def _distance(self, p1, p2):
+        return ((p1[0]-p2[0])**2 + (p1[1]+p2[1])**2)**0.5
+
+    def pick_players(self, court_kp, player_dt):
+        first_player_dt = player_dt[0]
+        players = self.choose(court_kp, first_player_dt)
+        players_filter = []
+        
+        for player_dict in player_dt: #we gonna search the players in the choose players
+            searched_players_filter = {track_id: box for track_id, box in player_dict.items() if track_id in players}
+            players_filter.append(searched_players_filter)
+        return players_filter
+
+    
+    def choose(self, court_kp, player_dt):
+        distances = []
+        for id, box in player_dt.items():
+            cx, cy = self._get_center(box)
+
+            min_dist = -999.9
+            for i in range(0, len(court_kp), 2):
+                court_points = (court_kp[i], court_kp[i+1])
+                distance = self._distance((cx, cy), court_points)
+                if distance < min_dist:
+                    min_dist = distance 
+            distances.append((id, min_dist))
+
+        distances.sort(key = lambda x: x[1])
+        players = [distances[0][0], distances[1][0]]
+        return players
 
     def detect_player(self, frames, read_stub = False, stub_path = None):
         player_dt = []
