9700c77941e1d7101bb86d0be704051a5c8ecb6b
Malk97sc <malaksanchez09@gmail.com>
2025-09-20 22:25:46 -0500

    Main Update

--- tennis_yolo/main.py
+++ tennis_yolo/main.py
@@ -1,3 +1,5 @@
+import cv2 as cv
+
 from pathlib import Path
 
 from utils import read_video, save_video
@@ -5,7 +7,7 @@ from utils import DATA_DIR, RAW_DATA_DIR, MODELS_DIR
 
 from tracking import PlayerTracking, BallTracking
 from court import CourtDetection
-from utils import refine_keypoints_subpix
+from utils import refine_keypoints_subpix, compute_player_stats, draw_player_stats
 
 def main():
     video_path = RAW_DATA_DIR / "input_video.mp4" 
@@ -19,6 +21,7 @@ def main():
     stub_path = MODELS_DIR / "tracker_stubs"    
 
     #--------read video-------
+    out_fps = 25
     height, width, fps, video = read_video(video_path)
     print(f"Height: {height}, Width: {width}")
     print(f"FPS: {fps}")
@@ -34,6 +37,10 @@ def main():
     order = 3 #this orders is only for Spline, cubicspline and Polynomial interpolation
     ball_dt = ball_track.interpolate_ball(ball_dt, method = interpolation_method, order = order) #ball interpolation to improve the result
 
+    #--------ball hit---------
+    ball_hit = ball_track.get_shot_frames(ball_dt, changes = 25)
+    #print(ball_hit)
+
     #-------court lines------------
     first_frame = video[0]
     court_line = CourtDetection(court_lines_model_path)
@@ -47,13 +54,19 @@ def main():
     #------pick Players---------
     player_dt = player_track.pick_players(court_kp, player_dt)
 
+    #------stats---------------
+    player_stats_df = compute_player_stats(player_dt, ball_dt, ball_hit, court_kp, out_fps)
+
     #-------draw boxes----------
     out_video = player_track.draw_boxes(video, player_dt, court_kp) 
-    out_video = ball_track.draw_boxes(video, ball_dt)
-    #out_video = court_line.draw_keypoints_on_video(video, court_kp)
+    out_video = ball_track.draw_boxes(out_video, ball_dt)
+    #out_video = court_line.draw_keypoints_on_video(out_video, court_kp)
+
+    #------draw stats----------
+    out_video = draw_player_stats(out_video, player_stats_df)
 
     output_video_path = output_path / f"output_video_{yolo_model}_{interpolation_method}.avi"
-    save_video(out_video, output_video_path)
+    save_video(out_video, output_video_path, out_fps)
     print(f"Save video in: {output_video_path}")
 
 if __name__ == "__main__":
