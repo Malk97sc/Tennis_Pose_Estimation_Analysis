f64db5a659d15da7ecfe5828d1437be32b63bca7
Malk97sc <malaksanchez09@gmail.com>
2025-09-21 09:49:01 -0500

    Main Update

--- tennis_yolo/main.py
+++ tennis_yolo/main.py
@@ -6,17 +6,20 @@ from utils import read_video, save_video
 from utils import DATA_DIR, RAW_DATA_DIR, MODELS_DIR
 
 from tracking import PlayerTracking, BallTracking
+from pose_estimation import PlayerPose
 from court import CourtDetection
 from utils import refine_keypoints_subpix, compute_player_stats, draw_player_stats
 
 def main():
-    video_path = RAW_DATA_DIR / "test_video.mp4" 
+    video_path = RAW_DATA_DIR / "input_video.mp4" 
     output_path = Path(DATA_DIR) / "results" / "video"
     output_path.mkdir(parents=True, exist_ok=True)
     
+    #--------models-----------
     yolo_model = "yolo11x.pt"
     player_model_path = MODELS_DIR / "yolo" / yolo_model
     ball_model_path = MODELS_DIR / "fine_tuning" / "ball_yolo11x" / "weights" / "best.pt"
+    pose_model_path = MODELS_DIR / "yolo_pose_estimation" / "yolo11x-pose.pt"
     court_lines_model_path = MODELS_DIR / "court_detection" / "keypoints_model.pth"
     stub_path = MODELS_DIR / "tracker_stubs"    
 
@@ -38,7 +41,7 @@ def main():
     ball_dt = ball_track.interpolate_ball(ball_dt, method = interpolation_method, order = order) #ball interpolation to improve the result
 
     #--------ball hit---------
-    ball_hit = ball_track.get_shot_frames(ball_dt, changes = 25)
+    #ball_hit = ball_track.get_shot_frames(ball_dt, changes = 25)
     #print(ball_hit)
 
     #-------court lines------------
@@ -54,6 +57,11 @@ def main():
     #------pick Players---------
     player_dt = player_track.pick_players(court_kp, player_dt)
 
+    #------pose estimation------
+    pose_estimator = PlayerPose(pose_model_path, conf_threshold = 0.001)
+    player_pose_dt = pose_estimator.estimate_pose(video, player_dt, read_stub = True, stub_path = stub_path / "pose_estimation.pkl")
+    #print(player_pose_dt)
+
     #------stats---------------
     #player_stats_df = compute_player_stats(player_dt, ball_dt, ball_hit, court_kp, out_fps)
 
@@ -61,11 +69,12 @@ def main():
     out_video = player_track.draw_boxes(video, player_dt, court_kp) 
     out_video = ball_track.draw_boxes(out_video, ball_dt)
     #out_video = court_line.draw_keypoints_on_video(out_video, court_kp)
+    out_video = pose_estimator.draw_pose(out_video, player_pose_dt)
 
     #------draw stats----------
     #out_video = draw_player_stats(out_video, player_stats_df)
 
-    output_video_path = output_path / f"output_video_{yolo_model}_{interpolation_method}.avi"
+    output_video_path = output_path / f"output_video_{yolo_model}_C{interpolation_method}_pose.avi"
     save_video(out_video, output_video_path, out_fps)
     print(f"Save video in: {output_video_path}")
 
